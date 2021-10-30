6350ed7d93c419e94ab29828c61b6dc9c6a55a17
Vitor Honna <vitorhonna@gmail.com>
2021-10-30 16:16:13 -0300

    JS notes

---
+++ soma.js
@@ -0,0 +1,20 @@
+// Exportando funções para WEB (adicionar type="module" na tag):
+/*
+
+export function soma(num1, num2) {
+  return num1 + num2;
+}
+
+*/
+
+
+// Exportando funções para NODE:
+/*
+
+module.exports = {
+  soma: function (num1, num2) {
+    return num1 + num2;
+  }
+}
+
+*/
