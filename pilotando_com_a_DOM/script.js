22d3d7757b920ca16c1414720ef86c2cf3af9d31
Vitor Honna <vitorhonna@gmail.com>
2021-11-09 13:18:35 -0300

    Update Notes Pilotando com a DOM

---
+++ pilotando_com_a_DOM/script.js
@@ -0,0 +1,132 @@
+const titleElement = document.getElementById("blog-title");
+console.log(titleElement);
+// Retorna um objeto com o elemento selecionado por ID (tipo Element)
+
+const textElements = document.getElementsByClassName("text");
+console.log(textElements);
+// Retorna uma lista de objetos que possuem a classe, mesmo que exista apenas 1 (tipo HTML Collection)
+console.log(textElements[0]);
+
+const metaElements = document.getElementsByTagName("body");
+console.log(metaElements);
+// Retorna uma lista de objetos que possuem a tag, mesmo que exista apenas 1 (tipo HTML Collection)
+
+// const element = document.querySelector("");
+// Recebe um seletor CSS e retorna o primeiro objeto que for encontrado (tipo Element):
+
+const titleElement2 = document.querySelector("#blog-title");
+console.log(titleElement2); // Retorna a mesma coisa que titleElement
+
+// const element = document.querySelectorAll("");
+// Recebe um seletor CSS e retorna uma lista com todos os objetos que forem encontrados (tipo Node List):
+
+const textElements2 = document.querySelectorAll(".text");
+console.log(textElements2); // Retorna uma lista semelhante a textElements, porém Node List
+
+// Node List possui o método "forEach()"
+// Referências sobre a diferença entre HTML Collection e Node List:
+// https://medium.com/@layne_celeste/htmlcollection-vs-nodelist-4b83e3a4fb4b
+// https://dev.to/jharteaga/difference-between-htmlcollection-and-nodelist-25bp
+
+// -----------------------------------------------------------------
+
+// https://github.com/vitorhonna/curso-prog-br#modificando-elementos
+
+/**
+ * textContent
+ * Recebe texto simples, retorna todo o texto que a tag contém, mesmo que algo esteja sendo escondido da visualização por meio de CSS, porém sem as tags.
+ */
+
+titleElement.textContent = "New title"; // Modifica o conteúdo de texto
+console.log(titleElement.textContent); // Também retorna o texto
+titleElement.textContent += " - Updates Concatenados"; // Usar += para concatenar
+
+/**
+ * innerText
+ * Recebe texto simples, retorna somente o texto visível que a tag contém.
+ */
+textElements[0].innerText = "<b>Lorem ipsum dolor sit amet<b>";
+
+/**
+ * innerHTML
+ * Recebe html e interpreta, retorna o conteúdo total, incluindo as tags.
+ */
+textElements[1].innerHTML = "<b>Lorem ipsum dolor sit amet<b>";
+
+/**
+ * value
+ * Altera o atributo "value"
+ */
+const inputElement = document.querySelector("input");
+console.log(inputElement);
+inputElement.value = "valor2";
+
+/**
+ * Outros atributos:
+ * setAttribute(<"attr">, <"value">)
+ * getAttribute(<"attr">)
+ * removeAttribute(<"attr">)
+ */
+
+titleElement.setAttribute("class", "title");
+console.log(titleElement); // Classe title adicionada
+console.log(titleElement.getAttribute("class")); // Retorna "title"
+titleElement.setAttribute("anyAttr", "value"); // Add atributo personalizado
+titleElement.removeAttribute("anyAttr"); // Remove atributo
+
+// -----------------------------------------------------------------
+
+// style
+titleElement.style.backgroundColor = "blue";
+titleElement.style.color = "white";
+
+// classList
+console.log(titleElement.classList); // Retorna um DOMTokenList de tamanho 1, com '0: "title"'
+titleElement.classList.add("green");
+console.log(titleElement.classList); // Retorna um DOMTokenList de tamanho 2, com '0: "title"' e '1: "green"'
+titleElement.classList.remove("green");
+console.log(titleElement.classList); // Retorna um DOMTokenList de tamanho 1, com '0: "title"'
+titleElement.classList.toggle("red"); // Se existe, remove. Se não existe, adiciona.
+console.log(titleElement.classList); // Retorna um DOMTokenList de tamanho 2, com '0: "title"' e '1: "red"'
+
+// -----------------------------------------------------------------
+
+const divElement = document.querySelector("#div");
+console.log(divElement);
+
+/* PAI: parentNode ou parentElement */
+console.log(divElement.parentNode); // Retorna o pai (Node): body
+console.log(divElement.parentElement); // Retorna o pai (Element): body
+
+/* FILHOS: childNodes, firstChild e lastChild ou children, firstElementChild e lastElementChild */
+console.log(divElement.childNodes); // Retorna uma NodeList com os filhos: tamanho 7 com texts (4) e tags span (3):
+// NodeList(7) [text, span, text, span, text, span, text] (antes de colocar os ids nos spans)
+// Se houver espaços/quebras de linha/textos, serão considerados "text" na lista de filhos
+
+console.log(divElement.children); // Retorna uma HTMLCollection com os filhos
+// Ignora os espaços/quebras de linha/textos puros: tamanho 3 com apenas tags span (3):
+// HTMLCollection(3) [span, span, span] (antes de colocar os ids nos spans)
+
+// Primeiro filho:
+console.log(divElement.firstChild); // Retorna o primeiro filho (Node): texto " Texto puro 1 " com as quebras de linha
+console.log(divElement.firstElementChild); // Retorna o primeiro filho (Element): tag span <span>Filho 1</span>
+
+// Último filho:
+console.log(divElement.lastChild); // Retorna o primeiro filho (Node): texto " Texto puro 4 " com as quebras de linha
+console.log(divElement.lastElementChild); // Retorna o primeiro filho (Element): tag span <span>Filho 3</span>
+
+/* IRMÃOS: nextSibling e previousSibling ou nextElementSibling e previousElementSibling */
+const child2 = document.querySelector("#child2");
+console.log(child2);
+
+// Irmão anterior
+console.log(child2.nextSibling); // (Node): texto " Texto puro 3 " com as quebras de linha
+console.log(child2.nextElementSibling); // (Element): tag span <span>Filho 3</span>
+
+// Irmão posterior
+console.log(child2.previousSibling); // (Node): texto " Texto puro 2 " com as quebras de linha
+console.log(child2.previousElementSibling); // (Element): tag span <span>Filho 1</span>
+
+console.clear();
+
+// -----------------------------------------------------------------
